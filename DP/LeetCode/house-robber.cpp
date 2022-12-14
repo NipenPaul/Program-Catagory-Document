3c100d3b42eeb384e752fee2b565c9b002336bfc
NipenPaul <53397507+NipenPaul@users.noreply.github.com>
2022-12-14 20:12:15 +0600

    Update house-robber.cpp

--- DP/LeetCode/house-robber.cpp
+++ DP/LeetCode/house-robber.cpp
@@ -1,3 +1,4 @@
+// iterative O(n^2)
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -12,5 +13,24 @@ public:
             ans = max(dp[i], ans);
         }
         return ans;
+    }
+};
+
+// recursion O(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        return max(rec(dp, n, 0, nums), rec(dp, n, 1, nums));
+    }
+    int rec(vector<int>&dp, int n, int ind,vector<int>&nums){
+        if(n <= ind) return -(int)2e9;
+        if(dp[ind] != -1) return dp[ind];
+        int res = nums[ind];
+        res = max(res, nums[ind]+rec(dp, n, ind+2, nums));
+        res = max(res, nums[ind]+rec(dp, n, ind+3, nums));
+        dp[ind] = res;
+        return dp[ind];
     }
 };
