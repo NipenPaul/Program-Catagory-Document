232616adc7cda519e2b50fa06f7331281b6953c5
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-04-06 23:05:00 +0600

    Update 1814B.cpp

--- Math/Codeforces/1814B.cpp
+++ Math/Codeforces/1814B.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void solution(){
   int x, y; cin >> x >> y;
   int ans = (x + y);
-  for(int i = 1; i <= (int)1e6; i++){
+  for(int i = 1; i <= (int)1e5; i++){
     ans = min(ans, i-1 + (x+i-1)/i + (y+i-1)/i);
   }
   cout << ans << '\n';
