a3a11997f72fd966beb90e0a55522aae625592f0
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-04-06 23:02:53 +0600

    Add files via upload

---
+++ Math/Codeforces/1814B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(){
+  int x, y; cin >> x >> y;
+  int ans = (x + y);
+  for(int i = 1; i <= (int)1e6; i++){
+    ans = min(ans, i-1 + (x+i-1)/i + (y+i-1)/i);
+  }
+  cout << ans << '\n';
+}
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while(t--){
+    solution();
+  }
+}
