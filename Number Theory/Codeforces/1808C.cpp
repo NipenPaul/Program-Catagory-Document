c927c3a139cc5502d085a2e6e51283d5c008fa08
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-04-17 01:31:40 +0600

    Update 1808C.cpp

--- Number Theory/Codeforces/1808C.cpp
+++ Number Theory/Codeforces/1808C.cpp
@@ -22,12 +22,12 @@ void solution(){
   int ans = l, p = 1;
   for(int i = 0; i <= 18; i++){
     for(int x = 0; x < 10; x++){
-      int v = l / (p * p) + ((p - 1) / 9) * x;
+      int v = (l / p) * p + ((p - 1) / 9) * x;
       //CHECK(v);
       if(l <= v && v <= r && cnt(ans) > cnt(v)){
         ans = v;
       }
-      v = r / (p * p) + ((p - 1) / 9) * x;
+      v = (r / p) * p + ((p - 1) / 9) * x;
       //CHECK(v);
       if(l <= v && v <= r && cnt(ans) > cnt(v)){
         ans = v;
