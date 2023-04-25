414ab1c07bdb120ffc0caafa54584ba0af69d5fb
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-04-25 17:06:52 +0600

    Create counting-bits.cpp

---
+++ DP/LeetCode/counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1, -1);
+        for(int i = 0; i <= n; i++){
+            if(dp[i] == -1){
+                rec(i, dp);
+            }
+        }
+        return dp;
+    }
+    int rec(int n, vector<int> &dp){
+        if(dp[n] != -1) return dp[n];
+        if(n == 0){
+            return dp[0] = 0;
+        }
+        return dp[n] = n % 2 + rec(n/2, dp);
+    }
+};
