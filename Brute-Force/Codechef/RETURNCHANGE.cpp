9385ef9529dc89a7706683ebb3133cad2f79c802
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-04-27 19:56:20 +0600

    Add files via upload

---
+++ Brute-Force/Codechef/RETURNCHANGE.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define CHECK(x)cout<<(#x)<<" is "<<(x)<<'\n';
+#define int long long
+
+void solution(){
+  int n; cin >> n;
+  int a = round(n/10.0);
+  cout << 100 - (a*10) << endl;
+}
+int32_t main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+  while(t--){
+    solution();
+  }
+  return 0;
+}
