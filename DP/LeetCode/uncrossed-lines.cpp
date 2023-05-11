87a101a4a905e5a6e5aab0175b064af4adfe65c7
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-05-11 15:59:49 +0600

    Update uncrossed-lines.cpp

--- DP/LeetCode/uncrossed-lines.cpp
+++ DP/LeetCode/uncrossed-lines.cpp
@@ -1,3 +1,4 @@
+// Approach 1: Recursive Dynamic Programming
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
@@ -15,5 +16,25 @@ public:
             return dp[i][j] = 1+rec(i+1, j+1, nums1, nums2, dp);
         }
         return dp[i][j] = max(rec(i, j+1, nums1, nums2, dp), rec(i+1, j, nums1, nums2, dp));
+    }
+};
+
+//Approach 2: Iterative Dynamic Programming
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
