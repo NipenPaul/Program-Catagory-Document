d08d254d83b9d55a86dc3e8c8c7ec1c43b610f61
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-05-12 11:41:27 +0600

    Update solving-questions-with-brainpower.cpp

--- DP/LeetCode/solving-questions-with-brainpower.cpp
+++ DP/LeetCode/solving-questions-with-brainpower.cpp
@@ -1,3 +1,4 @@
+// Recursion Solution
 class Solution {
 public:
     long long mostPoints(vector<vector<int>>& que) {
@@ -10,5 +11,24 @@ public:
        if(dp[ind][ok] != -1) return dp[ind][ok];
        long long res = max(que[ind][0]+rec(ind+que[ind][1]+1, que, n, dp, true), rec(ind+1, que, n, dp, true));
        return dp[ind][ok] = res; 
+    }
+};
+
+// Iterative solution
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& que) {
+        int n = que.size();
+        vector<long long> dp(n+1, 0);
+        dp[n-1] = que[n-1][0];
+        for(int i = n-2; i >= 0; i--){
+            dp[i] = que[i][0];
+            int skip = que[i][1];
+            if(skip+1+i < n){
+                dp[i] += dp[i+skip+1];
+            }
+            dp[i] = max(dp[i], dp[i+1]);
+        }
+        return dp[0];
     }
 };
