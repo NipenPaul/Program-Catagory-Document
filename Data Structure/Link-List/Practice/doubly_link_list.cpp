c4af1125bee738099d76c84bd6ad6877d22e3dc2
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-05-16 14:43:03 +0600

    Update doubly_link_list.cpp

--- Data Structure/Link-List/Practice/doubly_link_list.cpp
+++ Data Structure/Link-List/Practice/doubly_link_list.cpp
@@ -52,13 +52,42 @@ void insertLast(ListNode *&head, ListNode *&tail, int x){
   newNode->prev = tail;
   tail = newNode;
 }
+void deleteFirst(ListNode* &head, ListNode* &tail){
+  if(head == nullptr) return;
+  ListNode* tem = head;
+  if(head->next == NULL){
+    head = nullptr;
+    tail = head;
+    return;
+  }
+  head = head->next;
+  head->prev = nullptr;
+  delete(tem);
+}
+void deleteLast(ListNode* &head, ListNode* &tail){
+  if(tail == nullptr) return;
+  if(tail->prev == NULL){
+    tail = nullptr;
+    head = tail;
+    return;
+  }
+  ListNode *tem = tail;
+  tail = tail->prev;
+  tail->next = nullptr;
+  free(tem);
+}
 int main(){
-  ListNode *head, *tail;
+  ListNode *head = nullptr;
+  ListNode *tail = head;
   insertFirst(head, tail, 1);
   insertFirst(head, tail, 2);
   insertLast(head, tail, 5);
   insertFirst(head, tail, 3);
   insertLast(head, tail, 4);
+  deleteFirst(head, tail);
+  deleteFirst(head, tail);
+  insertLast(head, tail, 4);
+  deleteLast(head, tail);
   HeadShow(head);
   TailShow(tail);
   return 0;
