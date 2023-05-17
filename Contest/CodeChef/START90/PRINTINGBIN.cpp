c3442c3e0bbc4d7c49f2d17ef59aa69c975ec676
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-05-17 23:22:10 +0600

    Add files via upload

---
+++ Contest/CodeChef/START90/PRINTINGBIN.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define CHECK(x)cout<<(#x)<<" = "<<(x)<<'\n';
+#define int long long
+#define endl '\n'
+using namespace std;
+void solution(){
+  int n; cin >> n;
+  vector<int> v(n);
+  for(int i = 0; i < n; i++){
+    cin >> v[i];
+  }
+  for(int i = 0; i < n; i++){
+    cout << (v[i]^1) << " ";
+  } 
+  cout << endl;
+}
+int32_t main(){
+  ios::sync_with_stdio(false);cin.tie(nullptr);
+  int t = 1; cin >> t;
+  while(t--){
+    solution();
+  }
+  return 0;
+}
