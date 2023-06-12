d25ff0764bcba71a40aac063c93ea819f10b8ad8
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-06-12 18:58:14 +0600

    Create copyConstructor.cpp

---
+++ OOP/copyConstructor.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class constructor
+{
+private:
+  string name;
+  int id;
+  float cgp;
+public:
+  constructor(string name, int id, float cgp){
+    this->name = name;
+    this->id = id;
+    this->cgp = cgp;
+  }
+  constructor(const constructor &a){
+    name = a.name;
+    id = a.id;
+    cgp = a.cgp;
+  }
+  ~constructor(){
+
+  }
+  void display();
+};
+
+void constructor::display(){
+  cout << "Name: " << name << endl;
+  cout << "ID: " << id << endl;
+  cout << "CGPA: " << cgp << endl;
+  cout << endl;
+}
+
+int main(){
+  constructor cons("Nipen Paul", 174038, 3.13);
+  cons.display();
+  constructor cp = cons;
+  constructor cc = cp;
+  cc.display();
+  cp.display();
+  return 0;
+}
