bcae1abfab7d6781f6a24778fc3d226df0744d93
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-08-25 14:58:17 +0600

    Add files via upload

---
+++ Brute-Force/Codeforces/1862C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define CHECK(x)cout<<(#x)<<" = "<<(x)<<'\n';
+#define int long long
+#define endl '\n'
+using namespace std;
+void solution(){
+  int n; cin >> n;
+  vector<int> v(n);
+  for(int i = 0; i < n; i++){
+    cin >> v[i];
+  }
+  vector<int> ans;
+  for(int i = n-1; i >= 0; i--){
+    while(ans.size() < v[i]){
+      ans.push_back(i+1);
+    }
+  }  
+  if(ans == v) cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
+int32_t main(){
+  ios::sync_with_stdio(false);cin.tie(nullptr);
+  int t = 1; cin >> t;
+  while(t--){
+    solution();
+  }
+  return 0;
+}
