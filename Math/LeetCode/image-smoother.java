cfc91aa81fa51b2169737e12757f7191993c35fd
Nipen Paul <53397507+NipenPaul@users.noreply.github.com>
2023-12-19 18:08:26 +0600

    Create image-smoother.java

---
+++ Math/LeetCode/image-smoother.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length;
+        int n = img.length;
+        int [][] dr = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        int [][] ans = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int sum = img[i][j];
+                int cnt = 1;
+                for(int k = 0; k < dr.length; k++){
+                    int x = dr[k][0] + i;
+                    int y = dr[k][1] + j;
+                    if(x >= 0 && y >= 0 && x < m && y < n){
+                        cnt++;
+                        sum += img[x][y];
+                    }
+                }
+                ans[i][j] = sum / cnt;
+            }
+        }
+        return ans;
+    }
+}
